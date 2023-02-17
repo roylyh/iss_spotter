51a935b9a8a8c2dc7fbbca3c102ff353c45efc7e
roylyh <roy.louyihui@gmail.com>
2023-02-17 00:42:08 -0800

    refactoring

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchMyIp, fetchCoordsByIp} = require("./iss");
+const {nextISSTimesForMyLocation} = require("./iss");
 
 // fetchMyIp((error, ip) => {
 //   if (error) {
@@ -16,3 +16,26 @@ const {fetchMyIp, fetchCoordsByIp} = require("./iss");
 //   }
 //   console.log("It worked! Returned coordinates:", coordinates);
 // });
+
+// const exampleCoords = { latitude: 49.0743308, longitude: -122.5593218 };
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  // console.log(passTimes);
+  for (let passTime of passTimes) {
+    let date = new Date(passTime.risetime * 1000).toLocaleString();
+    // let date = new Date();
+    console.log(`Next pass at ${date} for ${passTime.duration} seconds!`);
+  }
+});
